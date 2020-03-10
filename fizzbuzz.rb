88839366feb454694d6fbb2660c182d942323365
Sonja Parsell <hello@sparsell.com>
2020-03-10 15:45:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(num)
     puts "Fizz"
     elsif num % 5 == 0
       puts "Buzz"
-    elsif (num % 5 == 0) && (num % 3 == 0)
+    elsif num % 5 == 0 && num % 3 == 0
       puts "Fizzbuzz"
     else
       "nothing"
