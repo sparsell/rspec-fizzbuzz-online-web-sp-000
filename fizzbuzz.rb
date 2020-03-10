41fa56a9e7ef6d489a91b9be42506d6e2d07af7e
Sonja Parsell <hello@sparsell.com>
2020-03-10 15:46:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3 == 0
-    puts "Fizz"
+  if num num % 5 == 0 && num % 3 == 0
+    puts "FizzBuzz"
     elsif num % 5 == 0
       puts "Buzz"
-    elsif num % 5 == 0 && num % 3 == 0
-      puts "Fizzbuzz"
+    elsif % 3 == 0
+      puts "Fizz"
     else
       "nothing"
   end
